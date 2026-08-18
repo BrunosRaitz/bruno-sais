22aa29b71fc63bdc4a67460e509abcbb8dedb280
BrunosRaitz <brunosrraitz1@gmail.com>
2026-08-18 19:04:27 -0300

    Exer05 feito

--- exercicio-metodos/src/Ex04.java
+++ exercicio-metodos/src/Ex04.java
@@ -13,6 +13,8 @@ public class Ex04 {
         }
 
         acharMaiorValor(vetor);
+        acharMenorValor(vetor);
+        acharMedia(vetor);
     }
     public static void acharMaiorValor(int [] vetor){
         int maior = vetor[0];
@@ -20,5 +22,22 @@ public class Ex04 {
             if (vetor[i] > maior) maior = vetor[i];
         }
         System.out.println("O maior valor é: " + maior);
+    }
+
+    public static void acharMenorValor(int [] vetor){
+        int menor = vetor[0];
+        for (int i = 0; i < vetor.length; i++){
+            if (vetor[i] < menor) menor = vetor[i];
+        }
+        System.out.println("O menor valor é: "+menor);
+    }
+
+    public static void acharMedia(int [] vetor){
+        double soma = 0;
+        for (int i = 0; i < vetor.length; i++){
+            soma += vetor[i];
+        }
+        double media = soma / vetor.length;
+        System.out.println("A media é: "+ media);
     }
 }
