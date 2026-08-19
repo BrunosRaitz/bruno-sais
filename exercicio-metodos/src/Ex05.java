f7bb9dc30067102473a24af4edd22d885d632fa4
BrunosRaitz <brunosrraitz1@gmail.com>
2026-08-19 19:04:36 -0300

    Update Ex05.java

--- exercicio-metodos/src/Ex05.java
+++ exercicio-metodos/src/Ex05.java
@@ -32,6 +32,30 @@ public class Ex05 {
             System.out.println(valor[1]+" com distancia de: "+distancia1 +"\n"+ valor[2]+" com distancia de: "+distancia2);
         }
     }
+  /* public static void obterNumeroPertoBase(int base, int num1, int num2) {
+
+       *
+        * Calcula a distância entre num1 e a base.
+        *
+        * Math.abs() retorna o valor absoluto de um número. Isso evita que a distância
+        * seja negativa.
+        *
+        * Exemplo: se num1 = 8 e base = 10:
+        *
+        * 8 - 10 = -2 Math.abs(-2) = 2
+        *
+       int distanciaBase1 = Math.abs(num1 - base);
+       int distanciaBase2 = Math.abs(num2 - base);
+
+       if (distanciaBase1 < distanciaBase2) {
+           System.out.println("O número mais perto é: " + num1);
+       } else if (distanciaBase2 < distanciaBase1) {
+           System.out.println("O número mais perto é: " + num2);
+       } else {
+           System.out.println("As distâncias são iguais");
+       }
+
+   }*/
 
 }
 
