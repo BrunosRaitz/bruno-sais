11e5ada3a8267ebb1d2ea8098a564263d4163167
BrunosRaitz <brunosrraitz1@gmail.com>
2026-08-25 20:49:35 -0300

    Realizado Exercicio 12 com 2 metodos

--- exercicio-metodos/src/Ex08.java
+++ exercicio-metodos/src/Ex08.java
@@ -5,6 +5,9 @@ public class Ex08 {
     public static double obterMediaNotas(double num1, double num2, double num3){
         double soma = num1 + num2 + num3;
         double media = soma / 3;
+        if (num1 < 0|| num1 > 10 || num2 < 0 || num2 > 10 || num3 < 0 || num3 > 10){
+            return 0;
+        }
         return  media;
     }
 
@@ -14,11 +17,11 @@ public class Ex08 {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Digite a 1º nota: ");
-        int nota1 = input.nextInt();
+        double nota1 = input.nextInt();
         System.out.println("Digite a 2º nota: ");
-        int nota2 = input.nextInt();
+        double nota2 = input.nextInt();
         System.out.println("Digite a 3º nota: ");
-        int nota3 = input.nextInt();
+        double nota3 = input.nextInt();
 
         System.out.println("Sua média é: " +obterMediaNotas(nota1, nota2, nota3));
     }
---
+++ exercicio-metodos/src/Ex12.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Ex12 {
+
+    public static String acharPosicaoSeParOuImpar(String palavra){
+
+        if (palavra.length() % 2 == 0){
+            return "É Par \nPrimeiro caractere é: [ "+palavra.charAt(0)+" ]";
+        }else {
+            return "É Impar \nSegundo caractere é: [ "+palavra.charAt(1)+" ]";
+        }
+    }
+    public static int contarPalavra(String palavra){
+        int tamanhoPalavra = palavra.length();
+        return  tamanhoPalavra;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=====================");
+        System.out.println("Digite uma palavra");
+        String palavra = input.nextLine();
+
+        System.out.println("Quantidade de letras: "+contarPalavra(palavra));
+        System.out.println(acharPosicaoSeParOuImpar(palavra));
+
+
+    }
+}
