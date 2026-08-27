802b6c91131f7ddd3e34ce212e8cbb6397fd055a
BrunosRaitz <brunosrraitz1@gmail.com>
2026-08-26 21:08:03 -0300

    Criado novo projeto para execicios de classes. Feito execicio 1

---
+++ exercicios-poo/src/Ex01.java
@@ -0,0 +1,19 @@
+public class Ex01 {
+
+    public static void main(String[] args) {
+
+        Usuario pessoa1 = new Usuario();
+        Usuario pessoa2 = new Usuario();
+
+        pessoa1.nome = "Thiago";
+        pessoa1.sexo = 'M';
+        pessoa1.nascimento = "30/12/2001";
+        pessoa1.estadoCivil = "Farmador de Aura";
+
+        pessoa2.nome = "Giusep Cadura";
+        pessoa2.sexo = 'M';
+        pessoa2.nascimento = "01/01/2026";
+        pessoa1.estadoCivil = "Betinha";
+
+    }
+}
---
+++ exercicios-poo/src/Usuario.java
@@ -0,0 +1,7 @@
+public class Usuario {
+
+    String nome;
+    char sexo;
+    String nascimento;
+    String estadoCivil;
+}
