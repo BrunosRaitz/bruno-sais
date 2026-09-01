116f29fcc576636023ee463f7a1446da04dcd8bf
BrunosRaitz <brunosrraitz1@gmail.com>
2026-09-01 20:44:40 -0300

    Exemplo de classe com metodo. quadrilatero

---
+++ aula-classe-com-metodo/src/Quadrilatero.java
@@ -0,0 +1,11 @@
+public class Quadrilatero {
+
+    double lado1;
+    double lado2;
+    double lado3;
+    double lado4;
+
+    public double obterPerimetro(){
+        return lado1 + lado2 + lado3 + lado4;
+    }
+}
---
+++ aula-classe-com-metodo/src/ativiadde1.java
@@ -0,0 +1,13 @@
+public class ativiadde1 {
+
+    public static void main(String[] args) {
+
+        Quadrilatero q1 = new Quadrilatero();
+        q1.lado1 = 5;
+        q1.lado2 = 5;
+        q1.lado3 = 5;
+        q1.lado4 = 5;
+
+        System.out.println(q1.obterPerimetro());
+    }
+}
