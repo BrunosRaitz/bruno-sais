5ac2bd2f254b9f466a2a2dfab87fe79c60d2ebb3
BrunosRaitz <brunosrraitz1@gmail.com>
2026-09-22 21:04:46 -0300

    Update Ex08.java

--- exercicios-poo/src/Ex08.java
+++ exercicios-poo/src/Ex08.java
@@ -2,10 +2,10 @@ public class Ex08 {
     public static void main(String[] args) {
 
         Contribuinte f1 = new Contribuinte("Jose", "12345678910", "AM", 30000);
-        Contribuinte f2 = new Contribuinte("Carlos", "9999999999", "SC", 32000);
+        Contribuinte f2 = new Contribuinte("Carlos", "99999999999", "SC", 32000);
         Contribuinte f3 = new Contribuinte("Felix", "67676767676", "SP", 100000);
-        Contribuinte f4 = new Contribuinte("Uelisson", "131313131313", "SP", 700000);
-        Contribuinte f5 = new Contribuinte("Ivandel", "112233445566", "SC", 40000);
+        Contribuinte f4 = new Contribuinte("Uelisson", "13131313131", "SP", 700000);
+        Contribuinte f5 = new Contribuinte("Ivandel", "11223344556", "SC", 40000);
 
         Contribuinte contribuintes[] = {f1, f2, f3, f4, f5};
 
