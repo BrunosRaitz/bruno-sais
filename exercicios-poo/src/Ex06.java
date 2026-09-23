23f5aa632b54cdd4296385448f59987e1d12b6e2
BrunosRaitz <brunosrraitz1@gmail.com>
2026-09-23 20:40:02 -0300

    Update Ex06.java

--- exercicios-poo/src/Ex06.java
+++ exercicios-poo/src/Ex06.java
@@ -2,34 +2,17 @@ public class Ex06 {
     public static void main(String[] args) {
 
 
-        Mercado[] lojas = new Mercado[3];
+        Mercado unidadeDeBlumenau = new Mercado("Fifo do bao", 100, 2.34, 55, 2.77);
+        Mercado unidadeDeJoinville = new Mercado("Atacadao do inferno", 142, 2.11, 67, 1.93);
+        Mercado unidadeDeFlorianopolis = new Mercado("Seu Eugebio", 341, 1.21, 455, 0.89);
 
-        lojas[0] = new Mercado("Fifo do bao", 100, 2.34, 55, 2.77);
-      //  lojas[0].nome = "Fifo do bao";
-      //  lojas[0].vendasM = 100;
-      //  lojas[0].precoM = 2.34;
-      //  lojas[0].vendasL = 55;
-      //  lojas[0].precoL = 2.77;
-
-        lojas[1] = new Mercado("Atacadao do inferno", 142, 2.11, 67, 1.93);
-      //  lojas[1].nome = "Atacadao do inferno";
-      //  lojas[1].vendasM = 142;
-      //  lojas[1].precoM = 2.11;
-      //  lojas[1].vendasL = 67;
-      //  lojas[1]. precoL = 1.93;
-
-        lojas[2] = new Mercado("Seu Eugebio", 341, 1.21, 455, 0.89);
-      //  lojas[2].nome = "Seu Eugebio";
-      //  lojas[2]. vendasM = 341;
-      //  lojas[2]. precoM = 1.21;
-      //  lojas[2]. vendasL = 455;
-      //  lojas[2].precoL = 0.89;
+        Mercado[] mercados = {unidadeDeBlumenau, unidadeDeJoinville, unidadeDeFlorianopolis};
 
         double maiorReceitaM = Double.MIN_VALUE;
         int posicaoM = 0;
-        for (int i = 0; i < lojas.length; i++){
+        for (int i = 0; i < mercados.length; i++){
 
-            double receita = lojas[i].calcularReceitaMaca();
+            double receita = mercados[i].calcularReceitaMaca();
             if (receita > maiorReceitaM){
                 maiorReceitaM = receita;
                 posicaoM = i;
@@ -39,41 +22,41 @@ public class Ex06 {
 
         double menorReceitaL = Double.MAX_VALUE;
         Mercado mercadoMenorReceitaLaranja = null;
-        for (int i = 0; i < lojas.length; i++){
-            double receita = lojas[i].calcularReceitaLaranja();
+        for (int i = 0; i < mercados.length; i++){
+            double receita = mercados[i].calcularReceitaLaranja();
             if (receita < menorReceitaL){
                 menorReceitaL = receita;
-                mercadoMenorReceitaLaranja = lojas[i];
+                mercadoMenorReceitaLaranja = mercados[i];
             }
         }
 
         double maiorReceitaTotal = 0;
         double segundaMaiorReceitaTotal = 0;
         Mercado mercadoSegundaMaiorReceitaTotal = null;
-        for (int i = 0; i <lojas.length; i++){
-            double receitaT = lojas[i].calcularReceitaTotal();
+        for (int i = 0; i <mercados.length; i++){
+            double receitaT = mercados[i].calcularReceitaTotal();
             if (receitaT > maiorReceitaTotal){
                 maiorReceitaTotal = receitaT;
             }
         }
-        for (int i = 0; i <lojas.length; i++){
-            double receitaT = lojas[i].calcularReceitaTotal();
+        for (int i = 0; i <mercados.length; i++){
+            double receitaT = mercados[i].calcularReceitaTotal();
             if (receitaT > segundaMaiorReceitaTotal && receitaT != maiorReceitaTotal){
 
                 segundaMaiorReceitaTotal = receitaT;
-                mercadoSegundaMaiorReceitaTotal = lojas[i];
+                mercadoSegundaMaiorReceitaTotal = mercados[i];
             }
         }
 
         double receitaGeralM = 0;
         double receitaGeralL = 0;
 
-        for (int i = 0; i <lojas.length; i++){
-            receitaGeralM += lojas[i].calcularReceitaMaca();
-            receitaGeralL += lojas[i].calcularReceitaLaranja();
+        for (int i = 0; i <mercados.length; i++){
+            receitaGeralM += mercados[i].calcularReceitaMaca();
+            receitaGeralL += mercados[i].calcularReceitaLaranja();
         }
 
-        System.out.println("A loja com maior receita  de Maça é: ["+lojas[posicaoM].nome+"]\nR$"+maiorReceitaM);
+        System.out.println("A loja com maior receita  de Maça é: ["+mercados[posicaoM].nome+"]\nR$"+maiorReceitaM);
 
         System.out.println("A loja com menor receita de Laranja é: ["+mercadoMenorReceitaLaranja.nome + "]\nR$"+menorReceitaL);
 
